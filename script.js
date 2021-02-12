9dbb47caea11e5efd601f6a4b3c7f85e38931c8b
richardsgab <richardsmariagabriela@gmail.com>
2021-02-12 17:15:21 +0100

    Adding click button & multiplier button

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-var hola = "camarones";
-<<<<<<< HEAD
-let choclo = 23456;
-=======
-var chao = "calamares";
->>>>>>> 75715bc0b3160b87c9fdb6992a62c781700e54c8
+var counter = 0;
+const points = 10;
+var score = 0;
+var multiplier= 1;
+var multiplierprice = 100;
+var autoclick;
+var bonus;
+
+
+document.getElementById("counter-lbl").innerHTML = counter;
+document.getElementById("score-lbl").innerHTML = score;
+document.getElementById("mult-btn-times").innerHTML = "x" + multiplier;
+document.getElementById("mult-btn-points").innerHTML = multiplierprice + "points";
+
+
+
+document.getElementById("cookie-btn").addEventListener("click", () => {
+   counter = counter + 1;
+   document.getElementById("counter-lbl").innerHTML = counter;
+   score = score + (points * multiplier);
+   document.getElementById("score-lbl").innerHTML = score;
+});
+
+document.getElementById("multiplier-btn").addEventListener("click", () => {
+   if( score >= multiplierprice){
+        score = score - multiplierprice;
+        document.getElementById("score-lbl").innerHTML = score;
+        multiplierprice = multiplierprice * 2;
+        multiplier = multiplier + 1; 
+        document.getElementById("mult-btn-times").innerHTML = "x" + multiplier;
+        document.getElementById("mult-btn-points").innerHTML = multiplierprice + "points";      
+   }
+});
+
+
