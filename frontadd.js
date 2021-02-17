e71c9e593753bb1257e2942abc89b11cdbb96565
Jann Basso <jannbasso0@gmail.com>
2021-02-17 10:19:48 +0100

    update plus one with text

--- frontadd.js
+++ frontadd.js
@@ -1,71 +1,40 @@
-/* OPTION 1 - ORIGIN FROM DIFFERENT PLACES */
+// PLUS ONE
 
-/*
-document.getElementById("map").onclick = function(){
-    // creating img +1 and positioning it
-    let floating = document.getElementById("floating-cookies")
-
-    let plusone = document.createElement("img")
-    plusone.setAttribute("src", "img/plusone.png")
-    plusone.style.position= "absolute"
-    plusone.style.top = "220px"
-    plusone.style.left = "45%"
-    floating.appendChild(plusone)
-   
-    // animating img
-    plusone.animate([
-        {
-            transform: "translateY(0px)",
-            opacity: 1 
-        },
-        {
-            transform: "translateY(-150px)",
-            opacity: 0.1
-        }
-    ], {
-        duration: 4000,
-        delay: 500
-        
-    })
-}
-*/
-
-
-/* OPTION 2 - ORIGIN FROM DIFFERENT PLACES */
-
-// creating img +1 and positioning it
+// creating +1
 let floating = document.getElementById("floating-cookies")
 
-let plusone1 = document.createElement("img")
-plusone1.setAttribute("src", "img/plusone.png")
+let plusone1 = document.createElement("p")
+plusone1.innerHTML = `+ ${multiplier}`
+plusone1.style.fontSize = "x-large"
 plusone1.style.position= "absolute"
 plusone1.style.top = "220px"
 plusone1.style.left = "45%"
 plusone1.style.opacity = "0"
 floating.appendChild(plusone1)
 
-let plusone2 = document.createElement("img")
-plusone2.setAttribute("src", "img/plusone.png")
+let plusone2 = document.createElement("p")
+plusone2.innerHTML = `+ ${multiplier}`
+plusone2.style.fontSize = "x-large"
 plusone2.style.position= "absolute"
 plusone2.style.top = "210px"
 plusone2.style.left = "50%"
 plusone2.style.opacity = "0"
 floating.appendChild(plusone2)
 
-let plusone3 = document.createElement("img")
-plusone3.setAttribute("src", "img/plusone.png")
+let plusone3 = document.createElement("p")
+plusone3.innerHTML = `+ ${multiplier}`
+plusone3.style.fontSize = "x-large"
 plusone3.style.position= "absolute"
 plusone3.style.top = "205px"
 plusone3.style.left = "40%"
 plusone3.style.opacity = "0"
 floating.appendChild(plusone3)
 
-
 document.getElementById("map").onclick = function(){
     let arrayPlusone = [plusone1, plusone2, plusone3]
     let randomPlusone = Math.floor(Math.random()*arrayPlusone.length)
 
-    // animating img
+    // animating +1
     arrayPlusone[randomPlusone].animate([
         {
             transform: "translateY(0px)",
